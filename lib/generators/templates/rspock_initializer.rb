58c55bb2d68ea4322454f274c471aca9eb6a02e8
Jean-Philippe Duchesne <jpduchesne89@gmail.com>
2018-08-24 17:10:43 -0400

    Add Rails backtrace silencer for lib/rspock

--- lib/generators/templates/rspock_initializer.rb
+++ lib/generators/templates/rspock_initializer.rb
@@ -1,2 +1,3 @@
 # frozen_string_literal: true
 Rails.backtrace_cleaner.add_filter { |line| RSpock::Backtrace.new.filter_location_string(line) }
+Ralis.backtrace_cleaner.add_silencer { |line| line =~ /#{Pathname.new("").join("lib", "rspock")}/ }
