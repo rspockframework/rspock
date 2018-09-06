af90f87b7619310a10d183d87aa80f476cdb5fa0
Jean-Philippe Duchesne <jpduchesne89@gmail.com>
2018-09-05 20:10:33 -0400

    Removed Rails silencers for RSpock

--- lib/generators/templates/rspock_initializer.rb
+++ lib/generators/templates/rspock_initializer.rb
@@ -1,4 +1,2 @@
 # frozen_string_literal: true
 Rails.backtrace_cleaner.add_filter { |line| RSpock::Backtrace.new.filter_location_string(line) }
-Rails.backtrace_cleaner.add_silencer { |line| line =~ /#{Pathname.new('').join('lib', 'ast_transform')}/ }
-Rails.backtrace_cleaner.add_silencer { |line| line =~ /#{Pathname.new('').join('lib', 'rspock')}/ }
